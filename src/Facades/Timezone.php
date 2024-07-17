eb5541871d6e79699d614d57ee9c5739af7c3798
Edwin van de Pol <developer@edwinvandepol.nl>
2024-07-17 11:05:42 +0200

    Add typehints to facade

--- src/Facades/Timezone.php
+++ src/Facades/Timezone.php
@@ -4,9 +4,22 @@ namespace Whitecube\LaravelTimezones\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void set(mixed $timezone = null)
+ * @method static void setCurrent(mixed $timezone = null)
+ * @method static \Carbon\CarbonTimeZone current()
+ * @method static void setStorage(mixed $timezone = null)
+ * @method static \Carbon\CarbonTimeZone storage()
+ * @method static \Carbon\CarbonInterface now()
+ * @method static \Carbon\CarbonInterface date(mixed $value, callable|null $maker)
+ * @method static \Carbon\CarbonInterface store(mixed $value, callable|null $maker)
+ */
 class Timezone extends Facade
 {
-    public static function getFacadeAccessor()
+    /**
+     * Get the registered name of the component.
+     */
+    public static function getFacadeAccessor(): string
     {
         return \Whitecube\LaravelTimezones\Timezone::class;
     }
